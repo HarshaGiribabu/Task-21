901e825a7d6f41585ee419afb20308a506a947d9
HarshaGiribabu <harshagiri1981@gmail.com>
2024-07-24 18:41:40 +0530

    Add files via upload

---
+++ frameid.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get('https://www.cowin.gov.in/')
+
+time.sleep(5)  
+
+faq_xpath = '//a[contains(text(), "FAQ")]'
+partners_xpath = '//a[contains(text(), "Partners")]'
+
+
+faq_element = driver.find_element(By.XPATH, faq_xpath)
+faq_element.click()
+
+
+partners_element = driver.find_element(By.XPATH, partners_xpath)
+partners_element.click()
+
+time.sleep(3)
+
+
+windows = driver.window_handles
+
+for window in windows:
+    driver.switch_to.window(window)
+    print(f"Window Title: {driver.title}, Frame ID: {window}")
+
+main_window = driver.window_handles[0]
+for window in windows:
+    if window != main_window:
+        driver.switch_to.window(window)
+        driver.close()
+
+driver.switch_to.window(main_window)
+
+print(f"Current Window Title: {driver.title}")
+
+driver.quit()
